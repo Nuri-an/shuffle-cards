a67df915e70831559da5c2b9430d49701806f71b
Nurian Coelho <nuriancoelho@hotmail.com>
2023-03-10 10:50:28 -0300

    feat(project): initial structure

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # shuffle-cards
 
 Essa aplicação consiste em um embaralhador de cartas construído em React. Especificidades são aplicadas nesse sistema.
+
+[Protótipo](https://www.figma.com/proto/qWyy9G2bEqp3mC7hMwG8Sv/Untitled?node-id=207%3A17&scaling=scale-down&page-id=207%3A2&starting-point-node-id=207%3A17)
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+describe('App Component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  it('should contain initial pattern text', () => {
+    expect(screen.getByText('Hello Word')).toBeTruthy();
+  });
 });
