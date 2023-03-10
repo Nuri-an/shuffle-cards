3e8014475eceb72c13e03912ea763fa8b68e9fba
Nurian Coelho <nuriancoelho@hotmail.com>
2023-03-10 18:21:46 -0300

    style(project): global theme

---
+++ src/styles/theme/index.ts
@@ -0,0 +1,42 @@
+export const defaultTheme = {
+  colors: {
+    brand: {
+      primaryLight: '#890F67CC',
+      primary: '#890F67',
+      secundaryLight: '#5B18B0CC',
+      secundary: '#5B18B0',
+    },
+
+    support: {
+      n01: '#D9D9D9',
+      n02: '#1B1B1B',
+    },
+
+    neutral: {
+      darkest: '#000000',
+      dark: '#636062CC',
+      medium: '#2D2D2D',
+      light: '#E6D2FF',
+      lightest: '#FFFFFF',
+    },
+  },
+  space: {
+    inset: {
+      xxs: 8,
+      xs: 12,
+      sm: 15,
+      md: 20,
+      lg: 30,
+      xl: 50,
+    },
+  },
+  border: {
+    radius: {
+      sm: 8,
+    },
+  },
+  weight: {
+    regular: 400,
+    bold: 700,
+  },
+};
