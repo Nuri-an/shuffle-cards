dd9baeb22baa8f82c0d0334e9ac8a0ab0940f320
Nurian Coelho <nuriancoelho@hotmail.com>
2023-03-12 19:30:17 -0300

    refactor(route): route for login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
-import AppRoutes from './routes';
+import Routes from './routes';
 import GlobalStyle from './styles/global';
 import { defaultTheme } from './styles/theme';
 import store from './redux/store';
 
-function App() {
+const App: React.FC = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       <BrowserRouter>
         <Provider store={store}>
-          <AppRoutes />
+          <Routes />
         </Provider>
       </BrowserRouter>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { Login } from 'src/screens';
 
 const AuthRoutes: React.FC = () => (
   <Routes>
     <Route path="*" element={<Navigate to="login" />} />
-    <Route path="login" element={<h1>Login</h1>} />
+    <Route path="login" element={<Login />} />
   </Routes>
 );
 
