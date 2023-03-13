c7d8f9badd818919cc269f22e1eddd427a2cd4d1
Nurian Coelho <nuriancoelho@hotmail.com>
2023-03-12 23:48:21 -0300

    test(login): use new Wrappers to test

--- src/screens/Login/Login.spec.tsx
+++ src/screens/Login/Login.spec.tsx
@@ -1,11 +1,15 @@
 import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { renderWithTheme } from 'src/utils/functions/tests';
+import { setupStore } from 'src/redux/rootReducer';
+import {
+  renderWithProviders,
+  renderWithReduxPreloader,
+} from 'src/utils/functions/tests';
 import Login from '.';
 
-describe('Button Component', () => {
+describe('Login Screen', () => {
   it('should render a initial components', () => {
-    renderWithTheme(<Login />);
+    renderWithProviders(<Login />);
     const loginScreen = screen.getByTestId('login');
     const loginImg = screen.getByTestId('login-img');
     const loginInput = screen.getByTestId('input');
@@ -16,14 +20,14 @@ describe('Button Component', () => {
   });
 
   it('should not render the button initially', () => {
-    renderWithTheme(<Login />);
+    renderWithProviders(<Login />);
     const loginButton = screen.getByTestId('login-button');
     expect(getComputedStyle(loginButton).visibility).toBe('hidden');
     expect(getComputedStyle(loginButton).opacity).toBe('0');
   });
 
   it('should render the button is visible when showing', async () => {
-    renderWithTheme(<Login />);
+    renderWithProviders(<Login />);
     const loginInput = screen.getByTestId('input');
     userEvent.type(loginInput, 'This is a test');
 
@@ -35,7 +39,7 @@ describe('Button Component', () => {
   });
 
   it('should render the button when type in input', async () => {
-    renderWithTheme(<Login />);
+    renderWithProviders(<Login />);
     const loginInput = screen.getByTestId('input');
     userEvent.type(loginInput, 'This is a test');
 
@@ -45,7 +49,7 @@ describe('Button Component', () => {
   });
 
   it('should return error message when click button and input empty', async () => {
-    renderWithTheme(<Login />);
+    renderWithProviders(<Login />);
     const loginButton = screen.getByTestId('button');
     await userEvent.click(loginButton);
 
@@ -54,15 +58,44 @@ describe('Button Component', () => {
     });
   });
 
-  it('should not return error message when click button and input thing', () => {
-    renderWithTheme(<Login />);
+  it('should not return error message when click button and input thing', async () => {
+    renderWithProviders(<Login />);
     const loginInput = screen.getByTestId('input');
-    userEvent.type(loginInput, 'This is a test');
+    await userEvent.type(loginInput, 'This is a test');
 
-    const loginButton = screen.getByTestId('login-button');
-    userEvent.click(loginButton);
+    const loginButton = screen.getByTestId('button');
+    await userEvent.click(loginButton);
 
     const loginError = screen.queryByTestId('login-error');
     expect(loginError).toBeNull();
+  });
+
+  it('should login user when click button', async () => {
+    const userName = 'Test User';
+    const previousState: SessionType = {
+      isLogged: false,
+      user: '',
+    };
+    const store = setupStore({ user: previousState });
+
+    renderWithReduxPreloader(<Login />, {
+      preloadedState: {
+        user: previousState,
+      },
+      store,
+    });
+
+    const loginInput = screen.getByTestId('input');
+    await userEvent.type(loginInput, userName);
+
+    const loginButton = screen.getByTestId('button');
+    await userEvent.click(loginButton);
+
+    expect(JSON.stringify(store.getState().user)).toBe(
+      JSON.stringify({
+        isLogged: true,
+        user: userName,
+      }),
+    );
   });
 });
